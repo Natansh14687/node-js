033fd28057ad3d1e4cb41cc0a20f964dbdc13cf9
Natansh14687 <natansh522@gmail.com>
2026-07-04 18:45:05 +0530

    HTTP methods and Postman api calls

--- src/app.js
+++ src/app.js
@@ -2,23 +2,29 @@ const express = require("express");
 
 const app = express();
 
-
-
-app.use("/hello",(req, res) => {
-    res.send("Hello jiiiiii  !!");
+// This route will dominate over all the http methods GET, POST, DELETE etc because is applicable for all methods and dominant so should be placed belo them or not placed if using HTTP methods
+app.use("/user", (req, res) => {
+  res.send("Hahahahahaha  !!!!");
 });
 
+// This will only handle GET calls to /user
+app.get("/user", (req, res) => {
+  res.send({ name: "Natansh", age: 22 });
+});
 
+app.post("/user", (req, res) => {
+  res.send("User added successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted !!");
+});
+
+// This will match all the HTTP method api calls to /test
 app.use("/test", (req, res) => {
-    res.send("This is test route");
+  res.send("This is test route");
 });
-
-app.use("/", (req, res) => {
-    res.send("Namaste Natansh");
-});
-
-
 
 app.listen(7777, () => {
-    console.log("App is successfully listening at port 7777");
-})
+  console.log("App is successfully listening at port 7777");
+});
