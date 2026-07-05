2d599bc780b05670004d7a1730afbfa6082656b0
Natansh14687 <natansh522@gmail.com>
2026-07-05 15:22:15 +0530

    error handling using try catch and err parameter of route handler

--- src/app.js
+++ src/app.js
@@ -4,26 +4,18 @@ const app = express();
 
 const { adminAuth, userAuth } = require("../middlewares/auth");
 
-// => middleware for authenticating admin route
-app.use("/admin", adminAuth);
-
-app.get("/user/login", (req, res, next) => {
-  console.log("Inside /user/login");
-  res.send("Response from /user/login route!!");
+app.get("/user/getData", (req, res, next) => {
+  try {
+    throw new Error("vvghvgv")
+    res.send("Helloooo");
+  } catch (err) {
+    next(err);
+  }
 });
 
-app.get("/user/getData", userAuth, (req, res, next) => {
-  console.log("This is the response from /user/getData route");
-  res.send("Response from /user/getData route !!");
-});
-
-app.get("/admin/getData", (req, res, next) => {
-  res.send("Getting the data from admin !");
-});
-
-app.delete("/admin/deleteData", (req, res, next) => {
-  res.send("Deleting the data from the admin !");
-});
+app.use((err, req, res, next)=>{
+  res.status(500).send("some error occured");
+})
 
 app.listen(7777, () => {
   console.log("App is successfully listening at port 7777");
