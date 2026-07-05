6ce430feb7abd86e9fca030b05fef493897ca2da
Natansh14687 <natansh522@gmail.com>
2026-07-05 12:00:07 +0530

    query and params in routing

--- src/app.js
+++ src/app.js
@@ -2,22 +2,26 @@ const express = require("express");
 
 const app = express();
 
-// This route will dominate over all the http methods GET, POST, DELETE etc because is applicable for all methods and dominant so should be placed belo them or not placed if using HTTP methods
-app.use("/user", (req, res) => {
-  res.send("Hahahahahaha  !!!!");
-});
+
 
 // This will only handle GET calls to /user
 app.get("/user", (req, res) => {
+  console.log(req.query);
   res.send({ name: "Natansh", age: 22 });
 });
 
-app.post("/user", (req, res) => {
+app.post("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
   res.send("User added successfully");
 });
 
 app.delete("/user", (req, res) => {
   res.send("User deleted !!");
+});
+
+// This route will dominate over all the http methods GET, POST, DELETE etc because is applicable for all methods and dominant so should be placed belo them or not placed if using HTTP methods
+app.use("/user", (req, res) => {
+  res.send("Hahahahahaha  !!!!");
 });
 
 // This will match all the HTTP method api calls to /test
